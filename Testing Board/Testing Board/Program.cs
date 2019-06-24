b9519f6c5d08e16a1ed4f37604d47bc1f01f1534
Kof223 <fidoko204@gmail.com>
2019-06-24 21:18:15 +0800

    Initial Commit

---
+++ Testing Board/Testing Board/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace Testing_Board
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /* loop forever */
+            while (true)
+            {
+
+                /* wait a bit */
+                System.Threading.Thread.Sleep(20);
+            }
+        }
+    }
+}
